ea5747f2f1a5335c20f3ec178dd3bb68b18f58bf
isha-paliwal3 <1032201235@mitwpu.edu.in>
2023-01-23 11:50:30 +0530

    edited navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,6 +16,7 @@ const Navbar = () => {
                     <ul id="menu">
                         <li><a href="#home">Home</a></li>
                         <li><a href="#about">About</a></li>
+                        <li><a href="#portfolio">Portfolio</a></li>
                         <li><a href="#resume">Resume</a></li>
                         <li><a href="#contact">Contact</a></li>
                     </ul>
@@ -29,6 +30,7 @@ const Navbar = () => {
                 <ul id="menu">
                     <li><a href="#home" onClick={() => setMenuIcon(false)}>Home</a></li>
                     <li><a href="#about" onClick={() => setMenuIcon(false)}>About</a></li>
+                    <li><a href="#portfolio" onClick={() => setMenuIcon(false)}>Portfolio</a></li>
                     <li><a href="#resume" onClick={() => setMenuIcon(false)}>Resume</a></li>
                     <li><a href="#contact" onClick={() => setMenuIcon(false)}>Contact</a></li>
                 </ul>
